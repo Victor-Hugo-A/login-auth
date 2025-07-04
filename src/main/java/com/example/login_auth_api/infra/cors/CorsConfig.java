d252e26757b460989f3fdddca6084391acc9b206
Victor-Hugo-A <victorpht.vh@gmail.com>
2025-07-04 11:53:51 -0300

    Cors

--- src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "DELETE", "PUT");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
     }
 }
