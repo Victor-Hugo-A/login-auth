5a486779c8b42db9b9de1f50329ed5c4cfcc0cf7
Victor-Hugo-A <victorpht.vh@gmail.com>
2025-07-04 15:13:51 -0300

    AuthControleDelete

--- src/main/java/com/example/login_auth_api/controllers/AuthController.java
+++ src/main/java/com/example/login_auth_api/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.login_auth_api.controllers;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.example.login_auth_api.domain.user.User;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,34 +40,40 @@ public class AuthController {
         return ResponseEntity.badRequest().build();
     }
 
-        @PostMapping("/register")
-            public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO body) {
-                // Verifica se já existe um usuário com o mesmo email no banco de dados
-                if (repository.findByEmail(body.email()).isPresent()) {
-                    return ResponseEntity
-                    .badRequest()
-                    .body(new ResponseDTO("Já existe um usuário cadastrado com esse EMAIL", null));
-                }
-
-                // Verfica se já existe usuário com o mesmo name no banco de dados
-                if (repository.findByName(body.name()).isPresent()) {
-                    return ResponseEntity
-                    .badRequest()
-                    .body(new ResponseDTO("Já existe um usuário cadastrado com esse NOME", null));
-                }
-                // Sua lógica de autenticação aqui
-                Optional<User> user = this.repository.findByEmail(body.email());
-
-                if(user.isEmpty()) {
-                User newUser = new User();
-                newUser.setPassword(passwordEncoder.encode(body.password()));
-                newUser.setEmail(body.email());
-                newUser.setName(body.name());
-                this.repository.save(newUser);
-
-                    String token = this.tokenService.generateToken(newUser);
-                    return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
-                }
-                return ResponseEntity.badRequest().build();
+    @PostMapping("/register")
+        public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO body) {
+            // Verifica se já existe um usuário com o mesmo email no banco de dados
+            if (repository.findByEmail(body.email()).isPresent()) {
+                return ResponseEntity
+                .badRequest()
+                .body(new ResponseDTO("Já existe um usuário cadastrado com esse EMAIL", null));
             }
+
+            // Sua lógica de autenticação aqui
+            Optional<User> user = this.repository.findByEmail(body.email());
+
+            if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+                String token = this.tokenService.generateToken(newUser);
+                return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+            }
+            return ResponseEntity.badRequest().build();
         }
+
+        @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDTO> deleteUser(@RequestBody Map<String, String> body) {
+        String email = body.get("email");
+        Optional<User> user = repository.findByEmail(email);
+        if (user.isPresent()) {
+            repository.delete(user.get());
+            return ResponseEntity.ok(new ResponseDTO("Usuário deletado com sucesso!", null));
+        } else {
+            return ResponseEntity.badRequest().body(new ResponseDTO("Usuário não encontrado.", null));
+        }
+    }
+}
